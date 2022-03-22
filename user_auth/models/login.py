3185472f069be2d9676e842489104a1a5f881861
shanweijia <454273687@qq.com>
2022-03-22 16:32:13 +0800

    feat(main): add support pdm
    添加PDM包管理支持

--- user_auth/models/login.py
+++ user_auth/models/login.py
@@ -11,19 +11,21 @@ if tp.TYPE_CHECKING:
 
 
 class LoginToken(SQLModel, table=True):
-    __tablename__ = "login_tokens"
+    __tablename__:str = "login_tokens" # type: ignore
     __table_args__ = (UniqueConstraint("token"),)
     id: int = Field(primary_key=True)
     token: str  # 校验token
     expired_in: typing.Optional[datetime.datetime]  # 过期时间
-    user_id: int = Field(foreign_key='user.id')  # 关联用户
+    user_id: int = Field(foreign_key="user.id")  # 关联用户
     user: User = Relationship(back_populates="tokens")
 
 
 class LoginLog(SQLModel, table=True):
-    __tablename__ = "login_logs"
+    __tablename__:str = "login_logs" # type: ignore
     id: int = Field(primary_key=True)
     user_id: int = Field(foreign_key="users.id", index=True)  # 关联用户
     user: User = Relationship(back_populates="login_logs", link_model="User")
-    login_time: datetime.datetime = Field(default_factory=lambda: datetime.datetime.utcnow)  # 登录时间
+    login_time: datetime.datetime = Field(
+        default_factory=lambda: datetime.datetime.utcnow
+    )  # 登录时间
     custom_ip: typing.Optional[ipaddress.IPv4Address]  # 登录用户IP
